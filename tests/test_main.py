8cc6cbdf0646e156c66d0dd89c783363896891a9
cds-amal <cds.sudama@gmail.com>
2023-08-01 11:09:25 -0400

    Fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,12 @@
 import pytest
-
-from src import main
+from main import add
 
 
 @pytest.mark.only
 def test_add():
-    assert main.add(1, 2) == 3
+    assert add(1, 2) == 3
 
 
 @pytest.mark.skip(reason="Not implemented yet")
 def test_subtract():
-    assert main.add(1, -2) == -1
+    assert add(1, -2) == -1
